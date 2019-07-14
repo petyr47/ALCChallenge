9be3d59071dd60c55e4fc1333026a288b0f5646c
petyr47 <anekepeter47@gmail.com>
2019-07-14 01:16:06 +0100

    handle ssl error 1 from webview

--- app/src/main/java/com/peter/aneke/alcchallenge/WebActivity.kt
+++ app/src/main/java/com/peter/aneke/alcchallenge/WebActivity.kt
@@ -1,14 +1,16 @@
 package com.peter.aneke.alcchallenge
 
+import android.net.http.SslError
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import android.webkit.WebSettings
+import android.view.View
+import android.webkit.*
 import kotlinx.android.synthetic.main.activity_web.*
 
 class WebActivity : AppCompatActivity() {
     
-    val url = "https://andela.com/alc"
+    private val url = "https://andela.com/alc/"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +21,24 @@ class WebActivity : AppCompatActivity() {
         supportActionBar?.setHomeButtonEnabled(true)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        alc_web_view.loadUrl(url)
-        alc_web_view.settings.javaScriptEnabled=true
-        //alc_web_view.settings.cacheMode= WebSettings.LOAD_CACHE_ELSE_NETWORK
+        val webView= alc_web_view as WebView
+
+
+        webView.webViewClient=object : WebViewClient(){
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                view?.loadUrl(url)
+                return true
+            }
+            override fun onReceivedSslError(view: WebView?, handler: SslErrorHandler?, error: SslError?) {
+                handler?.proceed()
+            }
+        }
+
+        webView.settings.javaScriptEnabled=true
+        webView.settings.cacheMode= WebSettings.LOAD_CACHE_ELSE_NETWORK
+
+
+        webView.loadUrl(url)
 
 
     }
